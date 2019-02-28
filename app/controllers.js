18c87c7c57c5564106415bca9da90c7165240c62
Tesfaye Gari <TGari@chemtrec.com>
2019-02-27 19:10:18 -0500

    Commit

--- app/controllers.js
+++ app/controllers.js
@@ -5,24 +5,27 @@
     controllerAs: 'vm',
     controller: function (svc) {
       var vm = this;
+      vm.$onInit = function () {
+        // TODO: Initialization goes here
+      };
       vm.tempUrl = svc.getTemplateUrl('carosel.html');
       vm.title1 = "This is Title 1";
     },
-    template: "<div ng-include='vm.tempUrl'></div>",    
+    template: "<div ng-include='vm.tempUrl'></div>",
     bindings: {
       title: '@'
     }
 
   });
 
-  
+
   mtmDemo.component('customfooter', {
     controllerAs: 'vm',
     controller: function (svc) {
       var vm = this;
       vm.tempUrl = svc.getTemplateUrl('footer.html');
     },
-    template: "<div ng-include='vm.tempUrl'></div>",    
+    template: "<div ng-include='vm.tempUrl'></div>",
     bindings: {
       title: '@'
     }
