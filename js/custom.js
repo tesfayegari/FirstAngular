d08553aa358d39976837be5363b8bd716cf7e0a1
Tesfaye Gari <TGari@chemtrec.com>
2019-03-01 12:02:57 -0500

    Initial css and js

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('#homeCarousel').carousel({
+        interval:3000,
+        pause: "false"
+    });
+    $('#playButton').click(function () {
+        $('#homeCarousel').carousel('cycle');
+    });
+    $('#pauseButton').click(function () {
+        $('#homeCarousel').carousel('pause');
+    });
+});
+  
+  
